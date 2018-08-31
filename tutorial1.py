713ea1fe250df3dca85cc5b73c47e39c3f9157b8
NIckLee-hanover <42844776+NIckLee-hanover@users.noreply.github.com>
2018-08-31 19:36:57 -0400

    Updated from Brython Server: 8/31/2018 7:36:56 PM

--- tutorial1.py
+++ tutorial1.py
@@ -2,7 +2,7 @@ from ggame import App, Color, LineStyle, Sprite
 from ggame import RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
 
 # color variabls
-red = Color(0xff0000, 1.0)
+red = Color(0xff0000, 0.7)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
@@ -13,13 +13,13 @@ thinline = LineStyle(1, black)
 # rectangle
 rectangle = RectangleAsset(50, 50, thinline, blue)
 ellipse = EllipseAsset(25, 25, thinline, blue)
-polygon = PolygonAsset([(10,60), (80,60), (90,0), (45,-30), (0,0)], thinline, red)
+polygon = PolygonAsset([(15,60), (75,60), (90,0), (45,-30), (0,0)], thinline, red)
 
 # display the shapes
 Sprite(rectangle, (0,0))
 Sprite(rectangle, (25,25))
 Sprite(ellipse, (100,100))
-Sprite(polygon, (80,90))
+Sprite(polygon, (80,80))
 
 myapp = App()
 myapp.run()
