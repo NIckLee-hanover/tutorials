4f308fbb1b7d35b92bdd74a8d5f0dd081ec87a89
NIckLee-hanover <42844776+NIckLee-hanover@users.noreply.github.com>
2018-08-31 17:31:01 -0400

    Updated from Brython Server: 8/31/2018 5:31:00 PM

--- tutorial1.py
+++ tutorial1.py
@@ -6,14 +6,16 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
 
 # 1 Pixel wide line
-thinline = LineStyle(1, black)
+thinline = LineStyle(1, white)
 # rectangle
 rectangle = RectangleAsset(50, 50, thinline, blue)
 
 # display a rectangle
-Sprite(rectangle, (100,100))
+Sprite(rectangle, (0,0))
+Sprite(rectangle, (25,25))
 
 myapp = App()
 myapp.run()
