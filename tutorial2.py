e2f48a919ae568068a054cd77f27908d4cfe54e3
NIckLee-hanover <42844776+NIckLee-hanover@users.noreply.github.com>
2018-09-02 12:04:21 -0400

    Updated from Brython Server: 9/2/2018 12:04:21 PM

--- tutorial2.py
+++ tutorial2.py
@@ -1,1 +1,22 @@
 
+from ggame import App, Color, LineStyle, Sprite
+from ggame import CircleAsset
+
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+
+thinline = LineStyle(1, black)
+bluecircle = CircleAsset(5,thinline, blue)
+whitecircle = CircleAsset(5,thinline, white)
+redcircle = CircleAsset(5,thinline, red)
+xcoordinates = range(100, 600, 10)
+
+sprites = [Sprite(redcircle, (x, x*0.5 + 100)) for x in xcoordinates]
+sprites = [Sprite(whitecircle, (x, x*0.5 + 120)) for x in xcoordinates]
+sprites = [Sprite(bluecircle, (x, x*0.5 + 140)) for x in xcoordinates]
+
+myapp = App()
+myapp.run()
